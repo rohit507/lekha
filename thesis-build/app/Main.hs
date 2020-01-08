1760faf309f09e70920fd327202ab5b79fff5004
Rohit Ramesh <rkr@berkeley.edu>
2020-01-08 14:21:13 -0800

    figured out structure for pandoc rules.

--- thesis-build/app/Main.hs
+++ thesis-build/app/Main.hs
@@ -2,7 +2,34 @@
 module Main where
 
 import Intro
-import Scratchpad
+import System.FSNotify
+import Pier.Core.Artifact
+import Development.Shake
+import Development.Shake.FilePath
+import Text.Pandoc
 
 main :: IO ()
-main = runLekha
+main = putStrLn "test"
+
+type DivID   = String
+type SpanID  = String
+type DivClass   = String
+type SpanClass  = String
+type Classes    = [String]
+type Params    = [(String, String)]
+
+-- Run the function for each classname from left to right, top down, until a
+-- fixed point is hit, should probably generalize this a bit using the 'walkable'
+-- class.
+type DivRule = (DivClass , DivFunc)
+type DivFunc = DivID   -> Params -> [Block]  -> Action (Format -> Action Block )
+
+type SpanRule = (SpanClass , SpanFunc)
+type SpanFunc = SpanID -> Params -> [Inline] -> Action (Format -> Action Inline)
+
+-- | Given some divRules and spanRules, will walk over a structure until it
+--   hits a fixed point (i.e. where none of the terms are raw blocks with
+--   a known class.
+applyRules :: (Walkable Block a, Walkable Inline a)
+           => ([DivRule], [SpanRule]) -> Format -> a -> Action a
+applyRules = undefined
--- thesis-build/src/Scratchpad.hs
+++ thesis-build/src/Scratchpad.hs
@@ -2,20 +2,18 @@
 module Scratchpad where
 
 import Intro
-import qualified Twitch as Twitch
-import qualified Development.Shake as Shake
-import qualified Development.Shake.FilePath as Shake
 
-type Rules a = ()
 
-runLekha :: RuleObj m =>  m-> IO ()
-runLekha m = do
-  frs <- fwdRules m
-  brs <- bwdRules m
-  if _watch
-  then _runFwdRules (frs *> _mkBwdRulesFwd brs)
-  else _runFwdRules frs *> _runBwdRules brs
+-- rule
+--  fwdRule
+--  bwdRule
+--
 
-class RuleObj m where
-  fwdRules :: m -> Twitch.Dep
-  bwdRules :: m -> Shake.Rules ()
+
+-- pandoc rule
+--  fromdiv  :: div  -> Action meta
+--  fromspan :: span -> Action meta
+--  todiv    :: meta -> Action pandoc
+--  tospan   :: meta -> Action inline
+--  fwdRules :: Twitch.Dev
+--  bwdRules :: Twitch.Bwd
